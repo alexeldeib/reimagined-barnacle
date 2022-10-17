a1ee176afa64805d5afae40e11b8ecebca9dab0f
Alexander Eldeib <alexeldeib@gmail.com>
2022-10-17 02:44:49 +0200

    more

--- get-token-ca.sh
+++ get-token-ca.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 #teleport configure -o stdout --roles node --auth-server port.alexeldeib.xyz:443 --token /opt/teleport/token.txt
-tsh login -o identity --proxy port.alexeldeib.xyz
+# tsh login -o identity --proxy port.alexeldeib.xyz
 cert="$(tctl --identity identity --auth-server port.alexeldeib.xyz status | rg "CA pin" | tr -s ' ' | cut -d' ' -f3)"
 token="$(tctl --identity identity --auth-server port.alexeldeib.xyz tokens add --type=node --ttl=1h | grep -oP '(?<=token:\s).*')"
 
